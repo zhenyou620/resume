6e83b52e4a4c6d40326dde899c7f4751f7244da8
m_sakaguchi <m_sakaguchi@guthrie.jp>
2024-06-13 09:44:59 +0900

    データ追加

---
+++ src/assets/zennScraps.ts
@@ -0,0 +1,327 @@
+{
+    "scraps": [
+      {
+        "id": 67058,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "35bf2eeace1663",
+        "title": "文字列を折り返したい。ついでに、末尾は...にしたい",
+        "closed": false,
+        "closed_at": null,
+        "archived": false,
+        "liked_count": 1,
+        "can_others_post": true,
+        "comments_count": 2,
+        "created_at": "2024-01-26T13:13:39.041+09:00",
+        "last_comment_created_at": "2024-01-26T13:14:52.355+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/35bf2eeace1663",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 65717,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "2b71224b5f2486",
+        "title": "いい感じのGoogle Fonts",
+        "closed": false,
+        "closed_at": null,
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": true,
+        "comments_count": 3,
+        "created_at": "2024-01-11T08:10:35.288+09:00",
+        "last_comment_created_at": "2024-03-26T13:21:34.898+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/2b71224b5f2486",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 57907,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "89b3a271022b6b",
+        "title": "MIT License",
+        "closed": true,
+        "closed_at": "2023-10-03T10:31:51.639+09:00",
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": true,
+        "comments_count": 7,
+        "created_at": "2023-10-02T17:07:45.213+09:00",
+        "last_comment_created_at": "2023-10-02T17:43:01.048+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/89b3a271022b6b",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 57562,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "bd924700e21df5",
+        "title": "Web Audio API",
+        "closed": false,
+        "closed_at": null,
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": true,
+        "comments_count": 8,
+        "created_at": "2023-09-27T17:21:43.534+09:00",
+        "last_comment_created_at": "2024-01-05T14:06:47.104+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/bd924700e21df5",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 57532,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "0d35c6489ae25e",
+        "title": "WebRTC通話の音量調整",
+        "closed": true,
+        "closed_at": "2023-09-27T17:20:00.717+09:00",
+        "archived": false,
+        "liked_count": 2,
+        "can_others_post": false,
+        "comments_count": 10,
+        "created_at": "2023-09-27T10:06:50.904+09:00",
+        "last_comment_created_at": "2023-09-27T17:19:54.182+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/0d35c6489ae25e",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 57526,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "7439301f797fee",
+        "title": "SDK",
+        "closed": true,
+        "closed_at": "2023-09-27T17:20:50.923+09:00",
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": true,
+        "comments_count": 2,
+        "created_at": "2023-09-27T09:07:06.726+09:00",
+        "last_comment_created_at": "2023-09-27T09:10:24.236+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/7439301f797fee",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 56634,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "992038735d1404",
+        "title": "ウィンドウ関数ってなんだ",
+        "closed": true,
+        "closed_at": "2024-01-05T13:37:45.491+09:00",
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": true,
+        "comments_count": 4,
+        "created_at": "2023-09-14T10:51:35.547+09:00",
+        "last_comment_created_at": "2023-09-14T11:31:33.906+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/992038735d1404",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 53518,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "fdc770a8d0868d",
+        "title": ".NET周りの関係がよくわからない",
+        "closed": true,
+        "closed_at": "2023-11-10T17:23:49.987+09:00",
+        "archived": false,
+        "liked_count": 1,
+        "can_others_post": false,
+        "comments_count": 8,
+        "created_at": "2023-07-31T10:09:40.686+09:00",
+        "last_comment_created_at": "2023-11-10T17:23:45.040+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/fdc770a8d0868d",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 52202,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "641a10bcecdd6b",
+        "title": "りあクト！TypeScriptで始めるつらくないReact開発 第4版【③React応用編】の勉強メモ",
+        "closed": true,
+        "closed_at": "2023-07-31T12:37:17.855+09:00",
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": false,
+        "comments_count": 3,
+        "created_at": "2023-07-11T12:48:14.791+09:00",
+        "last_comment_created_at": "2023-07-21T23:30:21.484+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/641a10bcecdd6b",
+        "topics": [],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 51043,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "443bb07caf9114",
+        "title": "りあクト！ TypeScriptで始めるつらくないReact開発 第4版【② React基礎編】の勉強メモ",
+        "closed": true,
+        "closed_at": "2023-07-11T12:49:26.448+09:00",
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": false,
+        "comments_count": 14,
+        "created_at": "2023-06-23T10:48:47.102+09:00",
+        "last_comment_created_at": "2023-07-11T12:46:44.999+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/443bb07caf9114",
+        "topics": [
+          {
+            "id": 1693,
+            "name": "jsx",
+            "taggings_count": 116,
+            "image_url": "https://zenn.dev/images/topic.png",
+            "display_name": "JSX"
+          },
+          {
+            "id": 99,
+            "name": "react",
+            "taggings_count": 8243,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/489b9436a3.png",
+            "display_name": "React"
+          },
+          {
+            "id": 122,
+            "name": "typescript",
+            "taggings_count": 8324,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/f13e758fdb.png",
+            "display_name": "TypeScript"
+          },
+          {
+            "id": 66,
+            "name": "javascript",
+            "taggings_count": 7431,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/64acd76870.png",
+            "display_name": "JavaScript"
+          }
+        ],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      },
+      {
+        "id": 50861,
+        "post_type": "Scrap",
+        "user_id": 123544,
+        "slug": "8c2e04cdc05cf0",
+        "title": "りあクト！TypeScriptで始めるつらくないReact開発 第4版【①言語・環境編】の勉強メモ",
+        "closed": true,
+        "closed_at": "2023-07-11T12:48:46.762+09:00",
+        "archived": false,
+        "liked_count": 0,
+        "can_others_post": false,
+        "comments_count": 17,
+        "created_at": "2023-06-20T17:24:00.734+09:00",
+        "last_comment_created_at": "2023-06-23T14:25:18.635+09:00",
+        "should_noindex": false,
+        "path": "/zhenyou620/scraps/8c2e04cdc05cf0",
+        "topics": [
+          {
+            "id": 154,
+            "name": "初心者",
+            "taggings_count": 3377,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/6eea895649.png",
+            "display_name": "初心者"
+          },
+          {
+            "id": 66,
+            "name": "javascript",
+            "taggings_count": 7431,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/64acd76870.png",
+            "display_name": "JavaScript"
+          },
+          {
+            "id": 122,
+            "name": "typescript",
+            "taggings_count": 8324,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/f13e758fdb.png",
+            "display_name": "TypeScript"
+          },
+          {
+            "id": 99,
+            "name": "react",
+            "taggings_count": 8243,
+            "image_url": "https://storage.googleapis.com/zenn-user-upload/topics/489b9436a3.png",
+            "display_name": "React"
+          }
+        ],
+        "user": {
+          "id": 123544,
+          "username": "zhenyou620",
+          "name": "mayu",
+          "avatar_small_url": "https://res.cloudinary.com/zenn/image/fetch/s--V4CHLuaw--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_70/https://storage.googleapis.com/zenn-user-upload/avatar/b9808f11ec.jpeg"
+        }
+      }
+    ],
+    "next_page": null
+  }
