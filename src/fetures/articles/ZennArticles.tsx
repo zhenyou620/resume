3b1057be145311debf2c371ee70d337c173f43de
may <bankouzhenyou8@gmail.com>
2024-06-16 23:54:48 +0900

    コンポーネント化

--- src/fetures/articles/ZennArticles.tsx
+++ src/fetures/articles/ZennArticles.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, type FC } from 'react';
-import dayjs from 'dayjs';
+import { ZennArticle } from '@/components/ZennArticle';
 import { type ZennArticleType } from '@/types/ZennArticleType';
-import { Card, CardContent } from '@/components/ui/card';
 import { getZennArticles } from '@/utils/getArticles';
 
 const ZennArticles: FC = () => {
@@ -14,8 +13,6 @@ const ZennArticles: FC = () => {
   //   void fetchArticles();
   // }, []);
 
-  const zennUrl = 'https://zenn.dev/';
-
   useEffect(() => {
     const result: ZennArticleType[] = getZennArticles();
     setArticles(result);
@@ -26,23 +23,7 @@ const ZennArticles: FC = () => {
       <h1 className="mb-4 mt-12 text-4xl font-semibold">Zenn</h1>
       <div className="mb-10 grid gap-x-2 gap-y-3 sm:grid-cols-2">
         {articles.map((article) => (
-          <a key={article.id} href={zennUrl.concat(article.path)}>
-            <Card className="pr-2">
-              <CardContent>
-                <div className="flex flex-row items-center gap-3 pt-4">
-                  <p className="p-2 text-6xl">{article.emoji}</p>
-                  <div className="flex flex-col">
-                    <h2 className="line-clamp-2 text-base font-bold tracking-tight">
-                      {article.title}
-                    </h2>
-                    <p className="text-xs font-medium text-gray-500">
-                      {dayjs(article.published_at).format('YYYY/MM/DD')}
-                    </p>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          </a>
+          <ZennArticle article={article} key={article.id}></ZennArticle>
         ))}
       </div>
     </section>
